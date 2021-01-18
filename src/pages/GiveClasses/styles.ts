09e78b70d8d408a5b613dc177e9e0e1c5a498f69
Eduardo <du_du.mba@hotmail.com>
2021-01-18 21:32:58 +0000

    create giveclasses page

--- src/pages/GiveClasses/styles.ts
+++ src/pages/GiveClasses/styles.ts
@@ -8,59 +8,43 @@ const styles = StyleSheet.create({
     padding: 40,
   },
 
-  banner: {
-    width: '100%',
-    resizeMode: 'contain',
+  content: {
+    flex: 1,
+    justifyContent: 'center',
   },
 
   title: {
-    fontFamily: 'Poppins_400Regular',
-    color: '#fff',
-    fontSize: 20,
-    lineHeight: 30,
-    marginTop: 80
-  },
-
-  titleBold: {
-    fontFamily: 'Poppins_600SemiBold'
-  },
-
-  buttonsContainer: {
-    flexDirection: 'row',
-    marginTop: 40,
-    justifyContent: 'space-between'
-  },
-
-  button: {
-    height: 150,
-    width: '48%',
-    borderRadius: 8,
-    padding: 24,
-    justifyContent: 'space-between',
-  },
-
-  buttonPrimary: {
-    backgroundColor: '#9871f5'
-  },
-
-  buttonSecondary: {
-    backgroundColor: '#04d361'
-  },
-
-  buttonText: {
     fontFamily: 'Archivo_700Bold',
     color: '#fff',
-    fontSize: 20,
+    fontSize: 32,
+    lineHeight: 37,
+    maxWidth: 180,
   },
 
-  totalConnections: {
-    fontFamily: 'Poppins_400Regular',
+  description: {
+    marginTop: 24,
     color: '#d4c2ff',
-    fontSize: 12,
-    lineHeight: 20,
-    maxWidth: 140,
-    marginTop: 40,
+    fontSize: 16,
+    lineHeight: 26,
+    fontFamily: 'Poppins_400Regular',
+    maxWidth: 240
+  },
+
+  okButton: {
+    marginVertical: 40,
+    backgroundColor: '#04d361',
+    height: 58,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8
+  },
+
+  okButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontFamily: 'Archivo_700Bold'
   }
+
 });
 
 export default styles;
